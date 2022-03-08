68cd6a969bb8776ad05cab67f53ef467b469dd35
jorgetunubala <jorgtunubala@gmail.com>
2022-03-07 20:44:02 -0500

    Creacion de nuestra primer app, curso spring java. Pasos hasta el video 14.

---
+++ src/test/java/com/platzy/market/PlatzyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzy.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatzyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
